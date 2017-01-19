2e47742d846cf1aa7fd4398b1d86ab236e2a5c27
Yaniv Bronhaim <ybronhei@redhat.com>
2017-01-19 12:29:18 +0200

    Adding logger container to flush all logs from vdsm-kube to stdout
    This patch also adds:
    1. docker-compose yml for debugging
    2. vdsm-kube-config.yaml - for defining env variables
    3. some comments to Dockerfile
    
    Change-Id: I763df028196af136bd26503bf1c0e21c3f58c3a4
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- master/scripts/add_network.py
+++ master/scripts/add_network.py
@@ -1,12 +1,13 @@
 #!/bin/python
 from vdsm import vdscli
+import os
 import socket
 
 c = vdscli.connect()
 network_attrs = {'nic': 'veth_name0',
                  'ipaddr': socket.gethostbyname(socket.gethostname()),
                  'netmask': '255.240.0.0',
-                 'gateway': '10.32.0.1',
+                 'gateway': os.environ['CLUSTER_GATEWAY'],
                  'defaultRoute': True,
                  'bridged': True}
 
